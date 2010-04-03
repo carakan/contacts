317d3bb0d9a22fef20be06061cf843280be092b8
Carlos Ramos <carakan@gmail.com>
2010-04-03 15:34:05 -0300

    Finished de user authentication

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,2 +1,6 @@
 class User < ActiveRecord::Base
+  acts_as_authentic do |c|
+    c.login_field = 'login'
+    
+  end
 end
--- db/schema.rb
+++ db/schema.rb
@@ -11,6 +11,16 @@
 
 ActiveRecord::Schema.define(:version => 20100403163215) do
 
+  create_table "sessions", :force => true do |t|
+    t.string   "session_id", :null => false
+    t.text     "data"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
+
+  add_index "sessions", ["session_id"], :name => "index_sessions_on_session_id"
+  add_index "sessions", ["updated_at"], :name => "index_sessions_on_updated_at"
+
   create_table "users", :force => true do |t|
     t.datetime "created_at"
     t.datetime "updated_at"
