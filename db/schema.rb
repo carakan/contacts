ef9112e1b17e3b51dfc7e3fd8755af39c4859e23
Carlos Ramos <carakan@gmail.com>
2010-04-03 16:00:34 -0300

    Added photo to each contact

--- db/schema.rb
+++ db/schema.rb
@@ -9,7 +9,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20100403184458) do
+ActiveRecord::Schema.define(:version => 20100403185728) do
 
   create_table "contacts", :force => true do |t|
     t.string   "name"
@@ -18,6 +18,10 @@ ActiveRecord::Schema.define(:version => 20100403184458) do
     t.string   "phone"
     t.datetime "created_at"
     t.datetime "updated_at"
+    t.string   "photo_file_name"
+    t.string   "photo_content_type"
+    t.integer  "photo_file_size"
+    t.datetime "photo_updated_at"
   end
 
   create_table "sessions", :force => true do |t|
